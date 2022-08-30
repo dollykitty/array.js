0774c1aaa1769665287aab583ea9a644b318cedc
dollykitty <99068668+dollykitty@users.noreply.github.com>
2022-08-30 17:28:40 +0530

    Add files via upload

---
+++ array.iniyaal.js
@@ -0,0 +1,16 @@
+var first_name = "Imiyaal";
+var last_name = "Anuram";
+concat()
+
+var a = 10;
+var b = 5;
+<><p>
+    write about visual studio and how to set it up
+</p><p>
+        write about console screen.And why it is use.And how to open console
+        screen and how to print anything on console
+    </p></>
+console.log()
+first_name.concat(last_name)
+var add = a + b ;
+console.log(add)
